adca583ab4f9ba6875c0c42d538d93cb783366d3
artinalushaku <al64778@ubt-uni.net>
2024-04-03 01:35:39 +0200

    Contact.tsx and contact.css UI not completely done

--- src/views/ContactUs.tsx
+++ src/views/ContactUs.tsx
@@ -1,17 +1,143 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/contact.css'
 
+function App() {
+  
+ 
+ 
+    return (
+    <div>
+      <div className="bar bg-dark text-white py-3">
+        <div className="container">
+          <h1 className="m-0">Welcome to ABODE: Where Hospitality Meets Home. Feel free to reach out to us. Your comfort is our priority.</h1>
+        </div>
+      </div>
+      <header className="sticky-top bg-light">
+        <div className="container d-flex justify-content-between align-items-center py-3">
+          
+          <div className="logo-2">
+            <a href="nav.html">
+              <img src="bars.png" alt="Menu" style={{ width: '30px' }} />
+            </a>
+          </div>
+          <nav>
+            <ul className="d-flex">
+              <li><a href="*">Home</a></li>
+              <li><a href="ContactUs.tsx">Contact</a></li>
+              <li><a href="*">About Us</a></li>
+            </ul>
+          </nav>
+         
+        </div>
+      </header>
 
+      <div className="contact py-5">
+        <div className="container">
+          <h1 className="text-center mb-4">Contact</h1>
+          <div className="row">
+            <div className="col-md-4 mb-4 mb-md-0">
+              <div className="con-info bg-light p-4">
+                <h3>GENERAL INFORMATION:</h3>
+                <p>Prishtina<br />
+                  65433561<br />
+                  Kosovo</p>
+              </div>
+            </div>
+            <div className="col-md-4 mb-4 mb-md-0">
+              <div className="con-info bg-light p-4">
+                <h3>CUSTOMER SUPPORT:</h3>
+                <p>Email: info@gmail.com<br />
+                  Phone: 65433561</p>
+                <div className="col">
+                  <h4>Monday through Friday</h4>
+                  <p>08:30 AM to 02:30 PM</p>
+                </div>
+              </div>
+            </div>
+           
+          </div>
+        </div>
+      </div>
 
-interface Props {}
+      <div className="last bg-light py-5">
+        <div className="container">
+          <form action="" method="post">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label htmlFor="fname">First Name</label>
+                  <input type="text" className="form-control" id="fname" name="fname" placeholder="Enter First Name" required />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lname">Last Name</label>
+                  <input type="text" className="form-control" id="lname" name="lname" placeholder="Enter Last Name" required />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" className="form-control" id="email" name="email" placeholder="Enter Email" required />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label htmlFor="Subject">Message</label>
+                  <textarea className="form-control" id="Subject" name="Subject" placeholder="Write your message..." style={{ height: '200px' }} required></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </div>
+            </div>
+            <div className="col-lg-6 right-image pr-0">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1619902.0054433027!2d-122.68851282163044!3d37.534535608111824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80859a6d00690021%3A0x4a501367f076adff!2sSan+Francisco%2C+CA%2C+USA!5e0!3m2!1sen!2sin!4v1507725785789"
+    width="100%" height="538" frameBorder="0" style={{ border: '0' }} allowFullScreen data-aos="fade-left" data-aos-duration="3000"></iframe>
+</div>
 
-const ContactUs = (props: Props) => {
-	
+          </form>
+        </div>
+      </div>
 
-	return (
-        <div>ContactUs</div>
-	);
-	
-	
+      <section className="footer bg-light">
+        <div className="container py-5">
+          <hr />
+          <div className="row">
+            <div className="col-md-3 mb-4 mb-md-0">
+              
+            </div>
+            <div className="col-md-3 mb-4 mb-md-0">
+              <div className="div">
+                <h3>stay connected with ABODE</h3>
+                <div className="icons">
+                  <img src="icons8-facebook-30.png" alt="Facebook" />
+                  <img src="icons8-instagram-30.png" alt="Instagram" />
+                  <img src="icons8-twitter-30.png" alt="Twitter" />
+                </div>
+                <h2>Mundësuar nga Abode, Inc.</h2>
+                <h5>Të gjitha të drejtat e rezervuara</h5>
+                <a href="">Kushtet e përdorimit – </a>
+                <a href="">Politika e Privatësisë</a>
+              </div>
+            </div>
+            <div className="col-md-3 mb-4 mb-md-0">
+              <div className="div">
+                <h3>Llogaria</h3>
+                <a href="">Rezervimet</a>
+                <a href="">Lista e deshirave</a>
+                <a href="">Llogaria ime</a>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="div">
+                <h3>Kontakti</h3>
+                <a href="">info@gmail.com</a>
+                <a href="">Tel:+383 045 883 702</a>
+                <a href="">Prishtinë, Kosovë</a>
+                <a href="">.......</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
 
-};
-export default ContactUs;
+export default App;
