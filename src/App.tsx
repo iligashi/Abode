c07168bb5f5dabdaa64b38ab3b1081e59a4587fc
Didirexha <153300036+Didirexha@users.noreply.github.com>
2024-04-07 00:52:43 +0200

    Deleting a useless path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./views/Homepage";
 import LoginView from "./views/LoginView";
-import ContactUs from "./views/ContactUs";
 import ProfileView from "./views/ProfileView";
 import SignUp from "./views/SignUp";
 import ContactForm from "./views/contactformTEST/contact-form-07/contact";
@@ -12,9 +11,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="home" element={<Homepage />} />
         <Route path="login" element={<LoginView />} />
-        <Route path="contactus" element={<ContactUs />} />
         <Route path="signup" element={<SignUp />} />
-        <Route path="contact" element={<ContactForm />} />
+        <Route path="contactus" element={<ContactForm />} />
 
         <Route
           path="profile"
