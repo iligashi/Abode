77c95aa0f3db95bf18dd1673902458076909f4a3
Didirexha <153300036+Didirexha@users.noreply.github.com>
2024-04-07 01:29:49 +0200

    Changes on Profile View

--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -70,7 +70,7 @@ const ProfileView: React.FC<Props> = ({ name, email, bio, phoneNumber, profileIm
   return (
     
      
-    
+    /* className="profile-picture-container"*/
     <div className="container">
       <Header/>
       <div className="row justify-content-center">
@@ -78,11 +78,26 @@ const ProfileView: React.FC<Props> = ({ name, email, bio, phoneNumber, profileIm
           <div className="card">
             <div className="card-body">
               <div className="profile-header text-center">
-                <div className="profile-picture-container">
+                <div style={{width: '100%'}}> 
+                  <div style={{display:'flex', justifyContent:'space-between', width:'100%',}}>
+
+                  <div style={{display:'block'}}>
+                    
+                 
                   <img src={profileImageUrl} className="profile-picture" alt="Profile" />
+                  <div className="profile-title">
+                                    <h2>{name}</h2>
+                                  </div>
+                                   </div>
+                   <div style={{alignSelf:'start',}}>
+
+                  
                   {!isEditing && (
                     <button className="btn btn-primary mt-2" onClick={handleEditProfile} style ={widthFix}>Edit Profile</button>
-                  )}
+                  )} 
+                  </div>
+
+                  </div>
                   {isEditing && (
                     <div className="change-image-button">
                       <label htmlFor="image-upload" className="btn btn-outline-primary mt-2">Change Image</label>
@@ -90,9 +105,7 @@ const ProfileView: React.FC<Props> = ({ name, email, bio, phoneNumber, profileIm
                     </div>
                   )}
                 </div>
-                <div className="profile-title">
-                  <h2>{name}</h2>
-                </div>
+               
               </div>
               <div className="profile-body">
                 <div className="profile-info">
