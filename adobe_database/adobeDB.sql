00edac8ba2f0dfd4f1b9cd6962c74bda6ea868fe
iligashi <119267734+iligashi@users.noreply.github.com>
2024-04-13 01:29:04 +0200

    Creating a table for User Profile

--- adobe_database/adobeDB.sql
+++ adobe_database/adobeDB.sql
@@ -22,3 +22,23 @@ INSERT INTO contact (fname, lname, email, subject) VALUES
 ('Mike', 'Johnson', 'mike.johnson@example.com', 'Feedback on customer service'),
 ('Emily', 'Brown', 'emily.brown@example.com', 'Technical support request'),
 ('David', 'Lee', 'david.lee@example.com', 'General inquiry');
+
+
+CREATE TABLE UserProfile (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    name NVARCHAR(50),
+    surname NVARCHAR(50),
+    email NVARCHAR(100),
+    bio NVARCHAR(MAX),
+    phoneNumber NVARCHAR(20),
+    profileImageUrl NVARCHAR(MAX),
+    status NVARCHAR(50)
+);
+INSERT INTO UserProfile (name, surname, email, bio, phoneNumber, profileImageUrl, status)
+VALUES
+('Ili', 'Gashi', 'ili.gashi@gashi.com', 'Passionate about technology and innovation.', '1234567890', 'http://ili.jpg/profile1.jpg', 'Active'),
+('Didi', 'Rexha', 'didi.rexha@gashi.com', 'Enthusiastic about programming and problem-solving.', '9876543210', 'http://didi.png/profile2.jpg', 'Inactive'),
+('Rina', 'Rina', 'rina.rina@gashi.com', 'Creative thinker with a passion for design and art.', '5551234567', 'http://rina.jpg/profile3.jpg', 'Active');
+
+
+SELECT * FROM UserProfile;
