fb9df550a9090175d2d4a8b03306f096277bee16
Didirexha <153300036+Didirexha@users.noreply.github.com>
2024-05-03 01:11:26 +0200

    Deleting previous Backend

--- abode/Program.cs
+++ abode/Program.cs
@@ -1,39 +1,35 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore.SqlServer;
-using abode.Models;
+using Microsoft.OpenApi.Models;
+using WorkingwithSQLLiteinAsp.NETCoreWebAPI.ApplicationDbContext;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to the container.
-builder.Services.AddDbContext<AbodeContext>(options =>
-{
-    {
-        options.UseSqlServer(builder.Configuration.GetConnectionString("Server=DESKTOP-H974K9N\\SQLEXPRESS;Database=adobe;Integrated Security=True;TrustServerCertificate=True;"));
-    };
-});
-
-
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Working with SQLLite In Asp.net Core Web API", Version = "v1" });
 
+});
 
-var app = builder.Build(); 
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.UseAuthorization();
 
-
+app.MapControllers();
 
 app.Run();
-
-
