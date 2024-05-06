d8e8ee5e0397de467c05ad61b7b9340e4f63adc7
iligashi <119267734+iligashi@users.noreply.github.com>
2024-05-07 01:28:18 +0200

    Comercial and Land Listing tables with controllers and Contexts

---
+++ abode/Migrations/LandListingDb/20240506232531_LandListing.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkingwithSQLLiteinAsp.NETCoreWebAPI.Migrations.LandListingDb
+{
+    /// <inheritdoc />
+    public partial class LandListing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LandListings",
+                columns: table => new
+                {
+                    ListingID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PropertyID = table.Column<int>(type: "INTEGER", nullable: false),
+                    SalePrice = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LandListings", x => x.ListingID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LandListings");
+        }
+    }
+}
---
+++ abode/Models/CommercialListing.cs
@@ -0,0 +1,14 @@
+﻿// Models/CommercialListing.cs
+using System.ComponentModel.DataAnnotations;
+using WorkingwithSQLLiteinAsp.NETCoreWebAPI.Models;
+
+public class CommercialListing
+{
+    [Key]
+    public int ListingID { get; set; }
+    public int PropertyID { get; set; }
+    public Property Property { get; set; } // Navigation property
+    public string Type { get; set; }
+    public decimal SalePrice { get; set; }
+    public decimal RentPrice { get; set; }
+}
