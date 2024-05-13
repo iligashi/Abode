5771b3b948034dad7f62fa194f9144cc8870e831
Didirexha <153300036+Didirexha@users.noreply.github.com>
2024-05-14 00:43:27 +0200

    Created a new userRegistration table

--- abode/Models/UserRegistraion.cs
+++ abode/Models/UserRegistraion.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿
 namespace WorkingwithSQLLiteinAsp.NETCoreWebAPI.Models
 {
-	public class UserRegistraion
-	{
-		public UserRegistraion()
-		{
-		}
-	}
+    public class UserRegistration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
 }
 
